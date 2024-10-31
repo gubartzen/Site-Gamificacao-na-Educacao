042be721576c0729a38e0d2a695c1763a286357b
gubartzen <108465498+gubartzen@users.noreply.github.com>
2024-10-31 11:49:54 -0300

    muita coisa

---
+++ jogo3.js
@@ -0,0 +1,18 @@
+function botaovoltar() {
+    location.href = "jogo3pagina.html";
+  
+  }
+
+function botaotreino() {
+   location.href = "jogo3treinamento.html";
+}
+
+function botaostart() {
+    alert("botão");
+}
+
+function botaovoltaum() {
+window.history.back();
+}
+
+ 
